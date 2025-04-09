ecfcb21dacd76fc6b24633c5715dc5a6895fda9e
Bader Almutlaq <badr11almutlaq@gmail.com>
2025-04-09 09:31:22 +0300

    changing names

--- test.py
+++
@@ -1,9 +0,0 @@
-with open("./data/mixed_dataset/metadata.txt", "r") as file:
-    lines = file.readlines()
-    new_lines = [lines[0]]
-    for i, line in enumerate(lines[1:], 1):
-        col = line.split(",")
-        col[0] = str(i)
-        new_lines.append(",".join(col))
-with open("./data/meatdata.txt", "w") as file:
-    file.writelines(new_lines)
