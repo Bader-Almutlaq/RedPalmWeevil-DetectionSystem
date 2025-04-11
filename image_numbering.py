c12038d2262aa9112d637a919a6775580ab1971b
Bader Almutlaq <badr11almutlaq@gmail.com>
2025-04-11 13:27:22 +0300

    modified the names of the folders

--- image_numbering.py
+++ image_numbering.py
@@ -1,30 +1,35 @@
+# This files separates the mixed dataset into two folders and numbers the images.
 import os
 import shutil
 
 folder = "./data/mixed_dataset"
-target_1 = "./data/insects.class"
-target_2 = "./data/rpw.class"
+target_1 = "./data/RPW.class"
+target_2 = "./data/NRPW.class"
 os.makedirs(target_1, exist_ok=True)
 os.makedirs(target_2, exist_ok=True)
 
-insect = []
 rpw = []
+nrpw = []
 
 for root, _, files in os.walk(folder):
     for file in files:
-        if "Insect" in file:
-            insect.append(file)
-        else:
+        if "RPW" in file:
             rpw.append(file)
+        else:
+            nrpw.append(file)
 
-for i, filename in enumerate(insect, 1):
+print(f"The number of NRPWs: {len(rpw)}")
+print(f"The number of NRPWs: {len(nrpw)}")
+
+
+for i, filename in enumerate(rpw, 1):
     source_path = os.path.join(folder, filename)
     _, ext = os.path.splitext(filename)
-    dest_path = os.path.join(target_1, f"insect-{i}{ext}")
+    dest_path = os.path.join(target_1, f"RPW-{i}{ext}")
     shutil.copy2(source_path, dest_path)
 
 for i, filename in enumerate(rpw, 1):
     source_path = os.path.join(folder, filename)
     _, ext = os.path.splitext(filename)
-    dest_path = os.path.join(target_2, f"rpw-{i}{ext}")
+    dest_path = os.path.join(target_2, f"NRPW-{i}{ext}")
     shutil.copy2(source_path, dest_path)
