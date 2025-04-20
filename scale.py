a2d4ce3113e25bd15722d8931c7e51d63cae3a84
Bader Almutlaq <badr11almutlaq@gmail.com>
2025-04-20 16:56:02 +0300

    update on scaling

--- scale.py
+++ scale.py
@@ -2,8 +2,8 @@ import os
 from PIL import Image
 
 # Input and output directories
-input_folder = "path_to_input_folder"
-output_folder = "path_to_output_folder"
+input_folder = "test/RPW-trap"
+output_folder = "test/RPW-trap-160"
 
 # Create the output folder if it doesn't exist
 if not os.path.exists(output_folder):
@@ -19,7 +19,7 @@ for filename in os.listdir(input_folder):
 
         # Open and resize the image
         with Image.open(input_path) as img:
-            img_resized = img.resize((1080, 1080))
+            img_resized = img.resize((160, 160))
 
             # Save the resized image to the output folder
             img_resized.save(output_path)
