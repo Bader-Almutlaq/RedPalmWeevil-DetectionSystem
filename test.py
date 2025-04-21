17f22677cc1722b2820ab09bb6bfa8a30d1b09b7
Bader Almutlaq <badr11almutlaq@gmail.com>
2025-04-21 23:03:13 +0300

    updates

--- test.py
+++ test.py
@@ -1,3 +1,29 @@
 import torch
-model = torch.load("saved_models/mobilenetv3.2_rpw.pth" ,map_location="cpu", weights_only=False)
+from torchvision import transforms
+
+MODEL_CONFIGS = {
+    "mobilenet": {
+        "classifier_index": 3,
+        "input_size": 224,
+    },
+    "efficientnet_b0": {
+        "classifier_index": 1,
+        "input_size": 224,
+    },
+    "efficientnet_b4": {
+        "classifier_index": 1,
+        "input_size": 380,
+    },
+}
+
+transform = transforms.Compose(
+    [
+        transforms.Resize((img_size, img_size)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
+model = torch.load(
+    "saved_models/mobilenetv3.2_rpw.pth", map_location="cpu", weights_only=False
+)
 print(type(model))
